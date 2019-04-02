d1a2f3481c7180ec3c193b917b463e34d486df11
Anshiii <bisexua1@foxmail.com>
2019-04-02 14:08:44 +0800

    fix: 修复格式;添加笔记

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,32 @@ export default {
   render
 };
 
+// export { createElement, Component, render };
+
+/* EXAMPLE */
 const root = document.getElementById("root");
 class App extends Component {
-    state={
-        name:" !"
-    }
+  state = {
+    name: " !"
+  };
 
-  click=()=>{
-      this.setState({
-          name:"???~"
-      })
-  } 
+  click = () => {
+    this.setState({
+      name: "???~"
+    });
+  };
 
   render() {
-    return createElement("div", {
-        onClick:this.click
-    }, "Hello ",this.props.name,this.state.name);
+    return createElement(
+      "div",
+      {
+        onClick: this.click
+      },
+      "Hello ",
+      this.props.name,
+      this.state.name
+    );
   }
 }
 const element = createElement(App, { name: "World" });
 render(element, root);
-
-// export { createElement, Component, render };
